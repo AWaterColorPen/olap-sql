6f012f9ab52d707df26cd9247b9519978fc933d5
jellyyang <jellyyang@tencent.com>
2021-10-19 15:41:46 +0800

    update

--- api/models/data_set.go
+++ api/models/data_set.go
@@ -10,8 +10,8 @@ import (
 var DefaultOlapSqlModelDataSetTableName = "olap_sql_model_data_sets"
 
 type DataSet struct {
-	ID          uint64         `yaml:"id"   			json:"id,omitempty"`
-	Name        string         `yaml:"name"    			json:"name"`
+	ID          uint64         `yaml:"id"               json:"id,omitempty"`
+	Name        string         `yaml:"name"             json:"name"`
 	Description string         `yaml:"description"      json:"description"`
 	Schema      *DataSetSchema `yaml:"schema"           json:"schema"`
 }
