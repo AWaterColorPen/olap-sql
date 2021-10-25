543657013e56b08efb5ce1fc3a55c138275e063b
QuteMelon <qutemelon@foxmail.com>
2021-10-25 15:11:19 +0800

    test

--- request_test.go
+++ request_test.go
@@ -2,22 +2,7 @@ package olapsql_test
 
 import (
 	"github.com/awatercolorpen/olap-sql/api/types"
-	"github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/assert"
-	"testing"
 )
-
-func TestRequest(t *testing.T) {
-	m, err := newManager(t)
-	assert.NoError(t, err)
-	assert.NoError(t, MockLoad(m))
-	request := Request1()
-	client, err := m.GetClients()
-	assert.NoError(t, err)
-	db, err := client.BuildDB(request)
-	logrus.Info(request.BuildSQL(db))
-}
-
 
 func Request1() *types.Request {
 	request := &types.Request{
